c0546ead30dd6854cb4c9bc3bbc6a25067e152bf
Mahshin Islam <126387570+mahshin1428@users.noreply.github.com>
2024-01-26 08:03:08 -0500

    Add files via upload

---
+++ A.love _story.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for i in range(t):
+     ss = input("")
+     s = "codeforces"
+
+     count = 0
+     for j in range(10):
+      if s[j] != ss[j] :
+         count +=1
+
+
+     print(count)
+
+
+
+
